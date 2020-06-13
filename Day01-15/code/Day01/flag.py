e4513da35ae97bd22a2449fdf15e124d19ae9c57
Keisawaakira <54810442+Keisawaakira@users.noreply.github.com>
2020-06-13 17:29:24 +0900

    Update flag.py

--- Day01-15/code/Day01/flag.py
+++ Day01-15/code/Day01/flag.py
@@ -5,24 +5,33 @@ import turtle
 
 
 def draw_rectangle(x, y, width, height):
+    # Python自定义函数
     """绘制矩形"""
     turtle.goto(x, y)
+    # 从当前位置直接按直线画到指定位置
     turtle.pencolor('red')
     turtle.fillcolor('red')
+    # 设定填充的颜色
     turtle.begin_fill()
+    # 开始填充
     for i in range(2):
         turtle.forward(width)
         turtle.left(90)
         turtle.forward(height)
         turtle.left(90)
+    # 圈填充的范围，可以不下笔
     turtle.end_fill()
+    # 将上面的范围全部填充为填充颜色
+    # 填充满闭合的范围，若部分不闭合则从绘制的起点开始到绘制的终点引线，填充满闭合所有范围
 
 
 def draw_star(x, y, radius):
     """绘制五角星"""
     turtle.setpos(x, y)
     pos1 = turtle.pos()
+    # 返回海龟当前位置
     turtle.circle(-radius, 72)
+    # 以当前位置为端点，radius为半径，72为角度画部分圆
     pos2 = turtle.pos()
     turtle.circle(-radius, 72)
     pos3 = turtle.pos()
@@ -38,12 +47,15 @@ def draw_star(x, y, radius):
     turtle.goto(pos2)
     turtle.goto(pos5)
     turtle.end_fill()
+    # 通过circle确定五角星的五点，再顺次连接圈出五角星并填充
 
 
 def main():
     """主程序"""
     turtle.speed(12)
+    # 海龟速度
     turtle.penup()
+    # 抬起笔，默认按下
     x, y = -270, -180
     # 画国旗主体
     width, height = 540, 360
@@ -56,11 +68,15 @@ def main():
     turtle.forward(pice * 3)
     turtle.right(90)
     draw_star(turtle.xcor(), turtle.ycor(), pice * 3)
+    # 海龟的x，y坐标
     x_poses, y_poses = [10, 12, 12, 10], [2, 4, 7, 9]
     # 画小星星
     for x_pos, y_pos in zip(x_poses, y_poses):
+        # 将x_poses和y_poses打包为元组的列表，长度以最短为准，[(10,2),(12,4).....]
         turtle.goto(x + x_pos * pice, y + height - y_pos * pice)
         turtle.left(turtle.towards(center_x, center_y) - turtle.heading())
+        # 海龟初始朝向和海龟该点至目标点所成矢量的夹角
+        # heading为海龟的朝向
         turtle.forward(pice)
         turtle.right(90)
         draw_star(turtle.xcor(), turtle.ycor(), pice)
@@ -72,3 +88,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    # 只允许作为脚本直接执行，被调用时不执行
